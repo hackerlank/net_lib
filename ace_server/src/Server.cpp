6bf98a72b76e148d99ea48c094ca4a57f8ab6e87
元林虎 <linhu.yuan@morefuntek.com>
2016-08-19 10:01:04 +0800

    ace_server

---
+++ ace_server/src/Server.cpp
@@ -0,0 +1,22 @@
+
+
+#include "server.h"
+
+int serverHandler::open(void *p)
+{
+	if (super::open(p) == -1)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+int serverHandler::handle_input(ACE_HANDLE fd /*= ACE_INVALID_HANDLE*/)
+{
+	return 0;
+}
+
+int serverHandler::handle_output(ACE_HANDLE fd /*= ACE_INVALID_HANDLE*/)
+{
+	return 0;
+}
